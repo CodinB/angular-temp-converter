b0b40bf41dd7568c608d1cf68fee5c568c6c11ba
Briun Greene <briun.greene@gmail.com>
2020-07-11 19:50:54 -0700

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { TempConverterComponent } from './component/temp-converter/temp-converter.component';
+
+// valid URLS for the system
+const routes: Routes = [
+  {path:'',component:MainPageComponent},
+  {path:'login',component:UserLoginComponent},
+  {path:'converter', component:TempConverterComponent},
+  {path: 'user/register',component: UserRegisterComponent},
+  {path: 'user/list',component: UserListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
